64fafabdad4e83f323404541e25bfb07ea248c0c
Masi <sima.mironova@protonmail.com>
2020-11-27 17:26:21 +0200

    initial commit

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -3,67 +3,31 @@ import ReactDOM from 'react-dom'
 
 
 
-const Header = (props) => {
-  console.log(props)
-  return (
-        
-    <h1> {props.course}</h1> 
-       
-       )
-      }
-const Part = (props) => {
-  console.log(props)
-  return (
-  <div>      
-    <p> {props.part} {props.exercise}</p> 
-       </div>
-       )
-      }
 
-const Content = (props)  =>{
-  console.log(props)
-  return (
-  <>
-    <Part part={props.part1} exercise={props.exercise1} />
-    <Part part={props.part2} exercise={props.exercise2} />
-    <Part part={props.part3} exercise={props.exercise3}/>
-    </>
-        )
-      }
-const Total = (props) => {
-  return (
-  <div>      
-    <p> {props.total}</p> 
-      </div>
-        )
-        }
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Header course={course.name} />
-      <Content part1={course.parts[0].name} exercise1={course.parts[0].exercises} part2={course.parts[1].name} exercise2={course.parts[1].exercises} part3={course.parts[2].name} exercise3={course.parts[2].exercises} />
-      <Total total={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
-    </div>
-  )
+  return <Course course={course} />
 }
-
 ReactDOM.render(<App />, document.getElementById('root'))
